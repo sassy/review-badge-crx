0a8c65feaf2e18eb170af3c8a0e26e0696724d6b
Satoshi Watanabe <sassy.watanabe@gmail.com>
2021-03-31 11:23:56 +0900

    add ask-badkge

--- src/content_scripts.ts
+++ src/content_scripts.ts
@@ -6,7 +6,8 @@ const tribute = new Tribute({
         { key: "must", value: "![badge](https://img.shields.io/badge/review-must-red.svg)" },
         { key: "imo", value: "![badge](https://img.shields.io/badge/review-imo-orange.svg)" },
         { key: "nits", value: "![badge](https://img.shields.io/badge/review-nits-green.svg)" },
-        { key: "suggest", value: "![badge](https://img.shields.io/badge/review-suggestion-blue.svg)"}
+        { key: "suggest", value: "![badge](https://img.shields.io/badge/review-suggestion-blue.svg)"},
+        { key: "ask", value: "![badge](https://img.shields.io/badge/review-ask-blue.svg)"}
       ],
       selectTemplate: function(item) {
         return item.original.value;
