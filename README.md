240ae9eabd5b260192050f647b046397aa3f8fac
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-02-22 00:32:35 +0900

    modify badge color.

--- README.md
+++ README.md
@@ -26,11 +26,13 @@ type `!r` in textarea and select popup list.
 |label |badge |
 |-------|-------|
 |must |![badge](https://img.shields.io/badge/review-must-red.svg) |
-|should |![badge](https://img.shields.io/badge/review-should-blue.svg)|
-|imo |![badge](https://img.shields.io/badge/review-imo-orange.svg) |
-|nits |![badge](https://img.shields.io/badge/review-nits-green.svg) |
-|suggest |![badge](https://img.shields.io/badge/review-suggestion-blue.svg) |
+|should |![badge](https://img.shields.io/badge/review-should-yellow.svg)|
+|imo (in my opinion) |![badge](https://img.shields.io/badge/review-imo-orange.svg) |
+|nits (nitpick) |![badge](https://img.shields.io/badge/review-nits-green.svg) |
+|suggest |![badge](https://img.shields.io/badge/review-suggest-brightgreen.svg) |
 |ask |![badge](https://img.shields.io/badge/review-ask-blue.svg) |
+|good |![badge](https://img.shields.io/badge/review-good-pink.svg) |
+
 
 # Build and archive
 
--- src/content_scripts.ts
+++ src/content_scripts.ts
@@ -4,11 +4,12 @@ const tribute = new Tribute({
     trigger: "!r",
     values: [
         { key: "must", value: "![badge](https://img.shields.io/badge/review-must-red.svg)" },
-        { key: "should", value: "![badge](https://img.shields.io/badge/review-should-blue.svg)" },
-        { key: "imo", value: "![badge](https://img.shields.io/badge/review-imo-orange.svg)" },
-        { key: "nits", value: "![badge](https://img.shields.io/badge/review-nits-green.svg)" },
-        { key: "suggest", value: "![badge](https://img.shields.io/badge/review-suggestion-blue.svg)"},
-        { key: "ask", value: "![badge](https://img.shields.io/badge/review-ask-blue.svg)"}
+        { key: "should", value: "![badge](https://img.shields.io/badge/review-should-yellow.svg)" },
+        { key: "imo :In my opinion", value: "![badge](https://img.shields.io/badge/review-imo-orange.svg)" },
+        { key: "nits : nitpick", value: "![badge](https://img.shields.io/badge/review-nits-green.svg)" },
+        { key: "suggest", value: "![badge](https://img.shields.io/badge/review-suggest-brightgreen.svg)"},
+        { key: "ask", value: "![badge](https://img.shields.io/badge/review-ask-blue.svg)"},
+        { key: "good", value: "![badge](https://img.shields.io/badge/review-good-pink.svg)"}
       ],
       selectTemplate: function(item) {
         return item.original.value;
